693cf0208a4bfbd670868202c3160dba4b3fda0f
Abellarr <da.ortiz83@gmail.com>
2023-05-04 15:27:29 -0700

    Configured seed.js file to seed tables in database. Altered docker files to migrate and seed info to database upon build.

--- api/src/db/seed.js
+++ api/src/db/seed.js
@@ -39,5 +39,53 @@ const seedStudents = async () => {
     }
 }
 
+const seedCohort = async () => {
+    try {
+        await pool.query(`INSERT INTO cohorts(cohort_number, start, graduation, instructor) VALUES 
+        (19, '01/17/2023', '05/26/2023', 'Phil Witkin'),
+        (20, '03/27/2023', '07/21/2023', 'Danny Andrews') RETURNING *`);
+        console.log('Cohorts seeded successfully!')
+    } catch (error) {
+        console.error('Error seeding cohorts:', error);
+    }
+}
 
-seedStudents().then(() => pool.end());
+const seedAssessment = async () => {
+    try {
+        await pool.query(`INSERT INTO assessments(assess_name, type) VALUES 
+        ('DOM API Assessment', 'Assessment'),
+        ('Data Types', 'Checkpoint'),
+        ('Loops and Control Flow', 'Checkpoint'),
+        ('Functions', 'Checkpoint'),
+        ('Server and DB Assessment', 'Assessment'),
+        ('Server Side Assessment', 'Assessment'),
+        ('Event Listeners', 'Checkpoint'),
+        ('Arrays', 'Checkpoint'),
+        ('Objects', 'Checkpoint'),
+        ('React Assessment', 'Assessment') RETURNING *`);
+        console.log('Assessments seeded successfully!')
+    } catch (error) {
+        console.error('Error seeding assessments:', error);
+    }
+}
+
+const seedProject = async () => {
+    try {
+        await pool.query(`INSERT INTO projects(project_name, type) VALUES 
+        ('Command Line Mystery', 'Solo'),
+        ('Guessing Game', 'Solo'),
+        ('MCSP Hack-a-Thon', 'Solo'),
+        ('Front-End Project', 'Solo'),
+        ('Back-End MVP Project', 'Solo'),
+        ('Checkerboard', 'Solo'),
+        ('Front End Capstone', 'Group'),
+        ('System Design Capstone', 'Group'),
+        ('Blue Ocean', 'Group') RETURNING *`);
+        console.log('Projects seeded successfully!')
+    } catch (error) {
+        console.error('Error seeding projects:', error);
+    }
+}
+
+
+seedStudents().then(()=> seedCohort().then(()=> seedAssessment().then(()=> seedProject().then(()=> pool.end()))));
--- api/src/db/seed.sql
+++ api/src/db/seed.sql
@@ -1,28 +1,28 @@
-INSERT INTO cohorts(cohort_number, start, graduation, instructor) VALUES(19, '01/17/2023', '05/26/2023', 'Phil Witkin');
-INSERT INTO cohorts(cohort_number, start, graduation, instructor) VALUES(20, '03/27/2023', '07/21/2023', 'Danny Andrews');
+INSERT INTO cohorts(cohort_number, start, graduation, instructor) VALUES (19, '01/17/2023', '05/26/2023', 'Phil Witkin');
+INSERT INTO cohorts(cohort_number, start, graduation, instructor) VALUES (20, '03/27/2023', '07/21/2023', 'Danny Andrews');
 
 
-INSERT INTO assessments(assess_name, type) VALUES('DOM API Assessment', 'Assessment');
-INSERT INTO assessments(assess_name, type) VALUES('Data Types', 'Checkpoint');
-INSERT INTO assessments(assess_name, type) VALUES('Loops and Control Flow', 'Checkpoint');
-INSERT INTO assessments(assess_name, type) VALUES('Functions', 'Checkpoint');
-INSERT INTO assessments(assess_name, type) VALUES('Server and DB Assessment', 'Assessment');
-INSERT INTO assessments(assess_name, type) VALUES('Server Side Assessment', 'Assessment');
-INSERT INTO assessments(assess_name, type) VALUES('Event Listeners', 'Checkpoint');
-INSERT INTO assessments(assess_name, type) VALUES('Arrays', 'Checkpoint');
-INSERT INTO assessments(assess_name, type) VALUES('Objects', 'Checkpoint');
-INSERT INTO assessments(assess_name, type) VALUES('React Assessment', 'Assessment');
+INSERT INTO assessments(assess_name, type) VALUES ('DOM API Assessment', 'Assessment');
+INSERT INTO assessments(assess_name, type) VALUES ('Data Types', 'Checkpoint');
+INSERT INTO assessments(assess_name, type) VALUES ('Loops and Control Flow', 'Checkpoint');
+INSERT INTO assessments(assess_name, type) VALUES ('Functions', 'Checkpoint');
+INSERT INTO assessments(assess_name, type) VALUES ('Server and DB Assessment', 'Assessment');
+INSERT INTO assessments(assess_name, type) VALUES ('Server Side Assessment', 'Assessment');
+INSERT INTO assessments(assess_name, type) VALUES ('Event Listeners', 'Checkpoint');
+INSERT INTO assessments(assess_name, type) VALUES ('Arrays', 'Checkpoint');
+INSERT INTO assessments(assess_name, type) VALUES ('Objects', 'Checkpoint');
+INSERT INTO assessments(assess_name, type) VALUES ('React Assessment', 'Assessment');
 
 
-INSERT INTO projects(project_name, type) VALUES('Command Line Mystery', 'Solo');
-INSERT INTO projects(project_name, type) VALUES('Guessing Game', 'Solo');
-INSERT INTO projects(project_name, type) VALUES('MCSP Hack-a-Thon', 'Solo');
-INSERT INTO projects(project_name, type) VALUES('Front-End Project', 'Solo');
-INSERT INTO projects(project_name, type) VALUES('Back-End MVP Project', 'Solo');
-INSERT INTO projects(project_name, type) VALUES('Checkerboard', 'Solo');
-INSERT INTO projects(project_name, type) VALUES('Front End Capstone', 'Group');
-INSERT INTO projects(project_name, type) VALUES('System Design Capstone', 'Group');
-INSERT INTO projects(project_name, type) VALUES('Blue Ocean', 'Group');
+INSERT INTO projects(project_name, type) VALUES ('Command Line Mystery', 'Solo');
+INSERT INTO projects(project_name, type) VALUES ('Guessing Game', 'Solo');
+INSERT INTO projects(project_name, type) VALUES ('MCSP Hack-a-Thon', 'Solo');
+INSERT INTO projects(project_name, type) VALUES ('Front-End Project', 'Solo');
+INSERT INTO projects(project_name, type) VALUES ('Back-End MVP Project', 'Solo');
+INSERT INTO projects(project_name, type) VALUES ('Checkerboard', 'Solo');
+INSERT INTO projects(project_name, type) VALUES ('Front End Capstone', 'Group');
+INSERT INTO projects(project_name, type) VALUES ('System Design Capstone', 'Group');
+INSERT INTO projects(project_name, type) VALUES ('Blue Ocean', 'Group');
 
 
 
