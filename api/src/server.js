feeb8499ae979957360dc3611d57426809963b11
JesthenB <jesthenh@gmail.com>
2023-05-05 18:57:46 -0500

    added successful fetch path for project dropdown and cohort dropdown. Also added in access control in server.js

--- api/src/server.js
+++ api/src/server.js
@@ -2,11 +2,18 @@ import express from "express";
 
 import pg from "pg";
 
-const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 const app = express();
 
 app.use(express.json());
+
+// app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // // Set up the PostgreSQL connection
 // const pool = new Pool({
