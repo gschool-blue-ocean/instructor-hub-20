350b2209d16f85017bf609485d95ad8eef1591ac
Abellarr <da.ortiz83@gmail.com>
2023-05-11 13:18:13 -0700

    Completed Students component with all events (add, update, delete) tested and working locally.

--- client/src/components/Body/DashboardPage/Students/UpdateStudent.jsx
+++ client/src/components/Body/DashboardPage/Students/UpdateStudent.jsx
@@ -3,23 +3,31 @@ import CohortContext from '../../../Context/CohortContext.jsx';
 import '../../../../styles/Students.css';
 
 const UpdateStudent = ({stud, keyID, onAdd}) => {
-    const { stu_name, email, github } = stud;
+    const { id, stu_name, email, github } = stud;
     const { cohort } = useContext(CohortContext);
 
-    function handleCancel() {
+    useEffect(()=> {
+        valueReset();
+    }, [stud])
+
+    function valueReset() {
         const formName = document.getElementById(`updateName${keyID}`);
         const formEmail = document.getElementById(`updateEmail${keyID}`);
         const formGithub = document.getElementById(`updateGithub${keyID}`);
         formName.value = formName.defaultValue;
         formEmail.value = formEmail.defaultValue;
         formGithub.value = formGithub.defaultValue;
+    }
+
+    function handleCancel() {
+        valueReset();
         document.getElementById(keyID).style.display = "none";
     }
 
     function gatherValues() {
-        let name = document.getElementById(`updateName${keyID}`);
-        let email = document.getElementById(`updateEmail${keyID}`);
-        let gitHub = document.getElementById(`updateGithub${keyID}`);
+        const name = document.getElementById(`updateName${keyID}`);
+        const email = document.getElementById(`updateEmail${keyID}`);
+        const gitHub = document.getElementById(`updateGithub${keyID}`);
         let allValues = {};
         allValues.stu_name = name.value;
         allValues.email = email.value;
@@ -31,20 +39,20 @@ const UpdateStudent = ({stud, keyID, onAdd}) => {
     function handleClick() {
         const updateStud = gatherValues();
         console.log(updateStud);
-        // fetch('http://localhost:8000/students', 
-        // { 
-        //     method: 'PUT',
-        //     credentials: "same-origin",
-        //     headers: {"Content-Type": 'application/json'},
-        //     body: JSON.stringify(updateStud)
-        // })
-        // .then(response => response.json())
-        // .then(response => console.log(response))
-        // .then(onAdd());
-        handleCancel();
+        console.log(id);
+        fetch(`http://localhost:8000/students/${id}`, 
+        { 
+            method: 'PUT',
+            credentials: "same-origin",
+            headers: {"Content-Type": 'application/json'},
+            body: JSON.stringify(updateStud)
+        })
+        .then(response => console.log(response.json()))
+        .then(onAdd())
+        .then(handleCancel())
+        .catch(error => console.log(error));
     }
 
-  
     return (
       <div className='update-modal' id={keyID} onClick={handleCancel}>
         <div className='student-modal-form' onClick={e => e.stopPropagation()}>
