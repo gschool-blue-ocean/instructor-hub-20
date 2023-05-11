152022b4ea564614a53c65b4e96cc790a223e0f4
Spawn9986 <scouch9986@gmail.com>
2023-05-11 11:41:19 -0500

    updated ther dashboard and projects buttons on the nav bar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import '../../styles/NavBar.css';
-import GalvanizeLogo from '../../styles/svgs/galvanize.svg';
-import GalvanizeTextLogo from '../../styles/svgs/galvanize-text-logo.svg';
+import React, { useContext } from "react";
+import "../../styles/NavBar.css";
+import GalvanizeLogo from "../../styles/svgs/galvanize.svg";
+import GalvanizeTextLogo from "../../styles/svgs/galvanize-text-logo.svg";
+import CohortContext from "../Context/CohortContext";
 
 const NavBar = () => {
+  const { setBodyDisplay } = useContext(CohortContext);
   return (
-    <div id='navbar'>
+    <div id="navbar">
+      <img id="galvanize-logo" src={GalvanizeLogo} alt="galvanize logo" />
       <img
-        id='galvanize-logo'
-        src={GalvanizeLogo}
-        alt='galvanize logo'
-      />
-      <img
-        id='galvanize-text-logo'
+        id="galvanize-text-logo"
         src={GalvanizeTextLogo}
-        alt='galvanize text logo'
+        alt="galvanize text logo"
       />
 
-      <p id='navbar-title' data-testid="title">Instructor Hub</p>
+      <p id="navbar-title" data-testid="title">
+        Instructor Hub
+      </p>
 
-      <button>Dashboard</button>
-      <button>Projects</button>
+      <button onClick={() => setBodyDisplay("body")}>Dashboard</button>
+      <button onClick={() => setBodyDisplay("project")}>Projects</button>
       <button>Select Cohort Dropdown</button>
     </div>
   );
