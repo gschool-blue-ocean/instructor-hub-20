1c35ed94fa7e83f0a3d2d9ab453257803e22a7f3
Abellarr <da.ortiz83@gmail.com>
2023-05-12 11:18:18 -0700

    Revert "Updated login and register pages with some form items for submission. Committing for pull/merge purposes."
    This reverts commit 82b2680d590f11e2c55228ecc21018218be5efd1.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,25 +1,20 @@
-import React, { useState, Component } from 'react';
+import React, { Component } from 'react';
 import '../../styles/App.css';
 import NavBar from '../NavBar/NavBar';
 import Body from '../Body/Body';
 import Footer from '../Footer/Footer';
-import Login from '../Login/Login.jsx';
 import { CohortProvider } from '../Context/CohortContext.jsx';
 
 
 
 const App = () => {
-  const [ userAuth, setUserAuth ] = useState(true);
-
   return (
     <div id='App'>
-      {!userAuth ? <Login userAuth={setUserAuth} /> :
       <CohortProvider>
         <NavBar></NavBar>
         <Body></Body>
         <Footer></Footer>
       </CohortProvider>
-      }
     </div>
   );
 };
--- client/src/components/Body/DashboardPage/Students/StudentTable.jsx
+++ client/src/components/Body/DashboardPage/Students/StudentTable.jsx
@@ -3,38 +3,49 @@ import React, { useState, useEffect } from 'react';
 
 import '../../../../styles/Students.css';
 
-const StudentTable = ({ stud }) => {
-  const { stu_name, cohort_number, email, github, graduation } = stud;
 
+const StudentTable = ({ stud, onAdd }) => {
 
-  return (
-    <div className='student-table'>
-      <table>
-        <tr className='student-table-header'>
-          <th>Name</th>
-          <th>Email</th>
-          <th>GitHub</th>
-          <th>Grad. Date</th>
-          <th>Cohort</th>
-        </tr>
-        {stud.map((val, key) => {
-          return (
-            <tr
-              key={key}
-              className='table-row'
-            >
-              <td className='table-data'>{val.stu_name}</td>
-              <td className='table-data'>{val.email}</td>
-              <td className='table-data'>{val.github}</td>
-              <td className='table-data'>{val.graduation}</td>
-              <td className='table-data'>{val.cohort_number}</td>
+    function handleDelete(num) {
+      let studNum = parseInt(num);
+      fetch(`http://localhost:8000/students/${studNum}`, 
+      { 
+          method: 'DELETE',
+          credentials: "same-origin",
+          headers: {"Content-Type": 'application/json'}
+      })
+      .then(response => response.json())
+      .then(onAdd());
+    }
+  
+    return (
+      <div className='student-table'>
+        <table>
+            <tr className='student-table-header'>
+              <th>Name</th>
+              <th>Email</th>
+              <th>GitHub</th>
+              <th>Graduation Date</th>
+              <th>Cohort</th>
+              <th></th>
             </tr>
-          );
-        })}
-      </table>
-    </div>
-  );
-};
+            {stud.map((val, key) => {
+                return (
+                    <tr className='table-row' key={`Student${key}`} onClick={()=> document.getElementById(`Student${key}`).style.display = "flex"}>
+                        <td>{val.stu_name}</td>
+                        <td>{val.email}</td>
+                        <td>{val.github}</td>
+                        <td>{val.graduation}</td>
+                        <td>{val.cohort_number}</td>
+                        <td><button className='delete-student' onClick={()=> handleDelete(val.id)}>Delete</button></td>
+                        <UpdateStudent keyID={`Student${key}`} stud={val} onAdd={onAdd} />
+                    </tr>
+                )
+            })}
+        </table>        
+      </div>
+    );
+  };
 
 
 export default StudentTable;
