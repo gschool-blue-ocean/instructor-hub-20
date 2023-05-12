435533e7756f46ef64e78ba661173e7383a28ff0
Abellarr <da.ortiz83@gmail.com>
2023-05-12 15:17:03 -0700

    Fixed some lost code from develop merge. Tested components to make sure they work.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import Login from '../Login/Login';
 import '../../styles/App.css';
 import NavBar from '../NavBar/NavBar';
 import Body from '../Body/Body';
@@ -8,15 +9,16 @@ import { CohortProvider } from '../Context/CohortContext.jsx';
 
 
 const App = () => {
-  const [ userAuth, setUserAuth ] = useState(false);
+  const [ userAuth, setUserAuth ] = useState(true);
 
   return (
     <div id='App'>
+      {!userAuth ? <Login /> :
       <CohortProvider>
         <NavBar></NavBar>
         <Body></Body>
         <Footer></Footer>
-      </CohortProvider>
+      </CohortProvider>}
     </div>
   );
 };
