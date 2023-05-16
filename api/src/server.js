89e9d0d1cb21841e8df5e0c01557aeb0c635c406
Abellarr <da.ortiz83@gmail.com>
2023-05-16 15:23:40 -0700

    Added functionality to Add New Cohort button on Dashboard menu. Tested and working, still needs form validation. Installed jsonwebtoken package and imported to server for creation of jwts for authorization.

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import bcrypt from "bcrypt";
 import pg from "pg";
+const jwt = require('jsonwebtoken');
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL
@@ -99,7 +102,8 @@ app.post('/login', async (req, res) => {
     if (!response.rows[0]) {
       res.status(404).send({ 'message': 'User not found'})
     } else if (await bcrypt.compare(password, response.rows[0].password)) {
-      res.status(200).send({ 'message': 'Login successful!'});
+      const token = jwt.sign({ email: response.rows[0].email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+      res.status(200).send({ token });
     } else {
       res.status(409).send({ 'message': 'Incorrect Password'})
     }
