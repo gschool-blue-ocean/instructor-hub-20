90f187b3762df9c6a924d664a1e2621ac1671f95
Spawn9986 <scouch9986@gmail.com>
2023-05-16 17:22:09 -0500

    added updates to the login page

--- client/src/components/Login/OrigLoginPage.jsx
+++ client/src/components/Login/OrigLoginPage.jsx
@@ -53,47 +53,33 @@ const LoginPage = ({ showReg, userAuth }) => {
 
   return (
     <div className="login-container">
-      <img className="login-image"
-        id="logo"
-        src="https://dotcom-files.s3.us-west-2.amazonaws.com/galvanize_logo_full-color_light-background.png"
-        alt="logo"
-      />
-        <h2 className="login-header">Sign In</h2>
-      <div>
-        <form className = 'sign-in-form'>
-          <h3 className="error-text">{errorLogin}</h3>
-          <input
-            type="email"
-            name="username"
-            className="login-input"
-            id="email-login"
-            placeholder="Email Address"
-          ></input>
-          <br></br>
-          <input
-            type="password"
-            name="password"
-            className="login-input"
-            id="password-login"
-            placeholder="Password"
-          ></input>
-          <br></br>
-        </form>
-      </div>
+      <h2 className="login-header">Sign In</h2>
+      <form>
+        <h3 className="error-text">{errorLogin}</h3>
+        <input
+          type="email"
+          name="username"
+          className="login-input"
+          id="email-login"
+          placeholder="Email Address"
+        ></input>
+        <br></br>
+        <input
+          type="password"
+          name="password"
+          className="login-input"
+          id="password-login"
+          placeholder="Password"
+        ></input>
+        <br></br>
+      </form>
       <div className="login-button-container">
-        <button className="sign-in-btn-primary" onClick={handleLogin}>
+        <button className="btn btn-primary" onClick={handleLogin}>
           Sign In
         </button>
-        <button className="login-button" onClick={handleReg}>
-          Register
-        </button>
-        <span class="forgetYourPW">Forgot your password?</span>
-        <br></br>
-        <span class="or-sign-in">or sign in using a service</span>
-        <div>
-          <a class="login-github-link" href="https://github.com">GITHUB</a>
-        </div>
+        {/* <button className='login-button' onClick={handleReg}>Register</button> */}
       </div>
+      <span class="or-sign-in">or sign in using a service</span>
     </div>
   );
 };
