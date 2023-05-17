b2c66d7ed1f0de26d2f65589b10680997e52504c
Spawn9986 <scouch9986@gmail.com>
2023-05-17 18:41:22 -0500

    temp

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
-import { AuthProvider } from 'react-auth-kit';
-import Login from '../Login/Login';
-import '../../styles/App.css';
-import NavBar from '../NavBar/NavBar';
-import Body from '../Body/Body';
-import Footer from '../Footer/Footer';
-import { CohortProvider } from '../Context/CohortContext.jsx';
-
-
+import React, { useState } from "react";
+import { AuthProvider } from "react-auth-kit";
+import Login from "../Login/Login";
+import "../../styles/App.css";
+import NavBar from "../NavBar/NavBar";
+import Body from "../Body/Body";
+import Footer from "../Footer/Footer";
+import { CohortProvider } from "../Context/CohortContext.jsx";
 
 const App = () => {
-  const [ userAuth, setUserAuth ] = useState(true);
+  const [userAuth, setUserAuth] = useState(true);
 
   return (
-    <div id='App'>
-      <AuthProvider
-      authType={'localstorage'}
-      authName={'_auth'}>
-        {!userAuth ? <Login userAuth={()=> setUserAuth(true)}/> :
-        <CohortProvider>
-          <NavBar></NavBar>
-          <Body></Body>
-          <Footer></Footer>
-        </CohortProvider>}
+    <div id="App">
+      <AuthProvider authType={"localstorage"} authName={"_auth"}>
+        {!userAuth ? (
+          <Login userAuth={() => setUserAuth(true)} />
+        ) : (
+          <CohortProvider>
+            <NavBar></NavBar>
+            <Body></Body>
+            <Footer></Footer>
+          </CohortProvider>
+        )}
       </AuthProvider>
     </div>
   );
