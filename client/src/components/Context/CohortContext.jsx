014d6dcc6e2da5892169ecc618bf904f333cbb5d
TDow520 <dow.tailon@gmail.com>
2023-06-13 14:47:36 -0500

    small mod to cohortContext.jsx

--- client/src/components/Context/CohortContext.jsx
+++ client/src/components/Context/CohortContext.jsx
@@ -4,7 +4,7 @@ const CohortContext = createContext();
 
 export const CohortProvider = ({ children }) => {
   const [cohort, setCohort] = useState('19');
-  const [bodyDisplay, setBodyDisplay] = useState('assessment');
+  const [bodyDisplay, setBodyDisplay] = useState('main');
 
   return (
     <CohortContext.Provider
