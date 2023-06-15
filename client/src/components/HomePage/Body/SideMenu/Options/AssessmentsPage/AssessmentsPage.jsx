f0aa089ac06f311e6fbb10b4da73f070e87cffb7
Jacob Phillips <0123phillips@gmail.com>
2023-06-15 23:40:15 +0100

    Jacob: Adds graph to home dashboard, along with adding a modal for the assessments card

--- client/src/components/HomePage/Body/SideMenu/Options/AssessmentsPage/AssessmentsPage.jsx
+++ client/src/components/HomePage/Body/SideMenu/Options/AssessmentsPage/AssessmentsPage.jsx
@@ -1,18 +1,15 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import styles from './AssessmentsPage.module.css';
-import AssessmentsTable from "./AssessmentsTable/AssessmentsTable";
-import CohortContext, {CohortProvider} from "../../../../../Context/CohortContext";
+import AssessmentsTable from './AssessmentsTable/AssessmentsTable';
+import CohortContext, { CohortProvider } from '../../../../../Context/CohortContext';
 
 const AssessmentsPage = () => {
-  
   const [assessments, setAssessments] = useState([]);
   const { cohort } = useContext(CohortContext);
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(
-        `http://localhost:8000/assessment_scores/${cohort}`
-      );
+      const response = await fetch(`http://localhost:8000/assessment_scores/${cohort}`);
       const assess = await response.json();
       setAssessments(assess);
     })();
@@ -20,26 +17,24 @@ const AssessmentsPage = () => {
   }, [cohort]);
 
   return (
-    <CohortProvider>
-    <div className={styles["student-container"]}>
-    <div className={styles["student-title"]}>
-        <span className={styles["title"]}>Assessments Dashboard</span>
+    <div className={styles['student-container']}>
+      <div className={styles['student-title']}>
+        <span className={styles['title']}>Assessments Dashboard</span>
       </div>
-    <div className={styles["table-container"]}>
-      <div className={styles["student-container-bar"]}>
-        <div className={styles["page-header"]}>
-          {`MCSP Assessments`}
+      <div className={styles['table-container']}>
+        <div className={styles['student-container-bar']}>
+          <div className={styles['page-header']}>{`MCSP Assessments`}</div>
+          <div className={styles['add-button-container']}></div>
+          <button className={styles['add-btn']}>Add Assessment</button>
         </div>
-        <div className={styles["add-button-container"]}></div>
-        <button className={styles["add-btn"]}>Add Assessment</button>
-      </div>
-      <div className={styles['table-cont']}>
-        <AssessmentsTable assessments={assessments} />
+        <div className={styles['table-cont']}>
+          <CohortProvider>
+            <AssessmentsTable assessments={assessments} />
+          </CohortProvider>
+        </div>
       </div>
     </div>
-  </div>
-  </CohortProvider>
-  )
-}
+  );
+};
 
 export default AssessmentsPage;
--- client/src/components/HomePage/Body/SideMenu/Options/AssessmentsPage/AssessmentsTable/AssessmentsTable.jsx
+++ client/src/components/HomePage/Body/SideMenu/Options/AssessmentsPage/AssessmentsTable/AssessmentsTable.jsx
@@ -1,31 +1,36 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import AssessmentCard from "./AssessmentCard/AssessmentCard";
-import styles from "./AssessmentsTable.module.css";
+import React, { useState, useEffect, useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import AssessmentCard from './AssessmentCard/AssessmentCard';
+import styles from './AssessmentsTable.module.css';
+import CohortContext from '../../../../../../Context/CohortContext';
 
 const AssessmentsTable = ({ assessments }) => {
+
+  const { setBodyDisplay } = useContext(CohortContext);
+
   const [sortConfig, setSortConfig] = useState({
-    key: "",
-    direction: "",
+    key: '',
+    direction: '',
   });
   const [selectedRow, setSelectedRow] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   const handleSort = (key) => {
-    let direction = "ascending";
-    if (sortConfig.key === key && sortConfig.direction === "ascending") {
-      direction = "descending";
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
     }
     setSortConfig({ key, direction });
   };
 
   const sortedAssessments = [...assessments].sort((a, b) => {
     const key = sortConfig.key;
-    const direction = sortConfig.direction === "ascending" ? 1 : -1;
+    const direction = sortConfig.direction === 'ascending' ? 1 : -1;
 
-    if (key === "student_name" || key === "assess_name") {
+    if (key === 'student_name' || key === 'assess_name') {
       return a[key].localeCompare(b[key]) * direction;
-    } else if (key === "grade" || key === "cohort_number") {
+    } else if (key === 'grade' || key === 'cohort_number') {
       return (a[key] - b[key]) * direction;
     }
 
@@ -36,6 +41,11 @@ const AssessmentsTable = ({ assessments }) => {
     setSelectedRow(index === selectedRow ? null : index);
   };
 
+  const handleCloseModal = () => {
+    setSelectedRow(null);
+  };
+  
+
   return (
     <div className={styles.tableContainer}>
       <div className={styles.tableHeader}>
@@ -43,35 +53,35 @@ const AssessmentsTable = ({ assessments }) => {
           <thead>
             <tr>
               <th className={styles.column1}>
-                Name{" "}
+                Name{' '}
                 <FontAwesomeIcon
                   icon={faCaretUp}
-                  className={styles["name-arrow"]}
-                  onClick={() => handleSort("student_name")}
+                  className={styles['name-arrow']}
+                  onClick={() => handleSort('student_name')}
                 />
               </th>
               <th className={styles.column5}>
-                Assessment{" "}
+                Assessment{' '}
                 <FontAwesomeIcon
                   icon={faCaretUp}
-                  className={styles["assessment-arrow"]}
-                  onClick={() => handleSort("assess_name")}
+                  className={styles['assessment-arrow']}
+                  onClick={() => handleSort('assess_name')}
                 />
               </th>
               <th className={styles.column6}>
-                Grade{" "}
+                Grade{' '}
                 <FontAwesomeIcon
                   icon={faCaretUp}
-                  className={styles["grade-arrow"]}
-                  onClick={() => handleSort("grade")}
+                  className={styles['grade-arrow']}
+                  onClick={() => handleSort('grade')}
                 />
               </th>
               <th className={styles.column6}>
-                Cohort{" "}
+                Cohort{' '}
                 <FontAwesomeIcon
                   icon={faCaretUp}
-                  className={styles["cohort-arrow"]}
-                  onClick={() => handleSort("cohort_number")}
+                  className={styles['cohort-arrow']}
+                  onClick={() => handleSort('cohort_number')}
                 />
               </th>
             </tr>
@@ -84,7 +94,7 @@ const AssessmentsTable = ({ assessments }) => {
             {sortedAssessments.map((assessment, index) => (
               <React.Fragment key={`Assessment_${index}`}>
                 <tr
-                  className={selectedRow === index ? styles.selectedRow : ""}
+                  className={selectedRow === index ? styles.selectedRow : ''}
                   onClick={() => handleRowClick(index)}
                 >
                   <td>{assessment.student_name}</td>
@@ -92,18 +102,21 @@ const AssessmentsTable = ({ assessments }) => {
                   <td>{assessment.grade}</td>
                   <td>{assessment.cohort_number}</td>
                 </tr>
-                {selectedRow === index && (
-                  <tr>
-                    <td colSpan="4">
-                      <AssessmentCard assessment={assessment} />
-                    </td>
-                  </tr>
-                )}
               </React.Fragment>
             ))}
           </tbody>
         </table>
       </div>
+      {selectedRow !== null && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <AssessmentCard
+              assessment={assessments[selectedRow]}
+              closeModal={handleCloseModal}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
